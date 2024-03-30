2d655bd672edcb3aa46090b2076f296a4400e49e
Leonardo Brito <leonardobritto.jr@gmail.com>
2024-03-29 22:46:30 -0400

    JS tipagem fraca

---
+++ tipagemFraca.js
@@ -0,0 +1,16 @@
+let qualquer = 'Legal'
+console.log(qualquer)
+console.log(typeof qualquer)
+
+qualquer = 3.1516
+console.log(qualquer)
+console.log(typeof qualquer)
+
+// a mesma variável pode ter diversos tipos, o que significa que JS é uma linguagem de tipagem fraca.
+
+//Evitar nomes genéricos e siglas
+let valor = ' ' 
+let numero = 1
+let pqp = false //produto quimico perigoso .. kkk
+
+// Prefira código auto explicativo a comentário de código

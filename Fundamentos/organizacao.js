0ce411b65849e487eed61e8d66bc7694363d98e6
Leonardo Brito <leonardobritto.jr@gmail.com>
2024-03-29 22:03:16 -0400

    organização de código JS

---
+++ Fundamentos/organizacao.js
@@ -0,0 +1,11 @@
+console.log('Sentença de código');
+
+
+// Bloco de código, entre chaves:
+{
+    console.log("Olá")
+    console.log("Mundo")    
+    {
+        //Pode haver blocos de códigos dentro de blocos de códigos;
+    }
+}

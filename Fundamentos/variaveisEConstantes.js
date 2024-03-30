52fdc83565e0a1aece4e6ab1ff7f469724da1f3d
Leonardo Brito <leonardobritto.jr@gmail.com>
2024-03-29 22:33:03 -0400

    variáveis e constantes em JS

---
+++ Fundamentos/variaveisEConstantes.js
@@ -0,0 +1,7 @@
+var a = 3 // var(palavra reservada que cria uma variavel) denominada "a" recebe (=) o valor 3
+let b = 4 //let veio para resolver o problema que ocorre com o var. Com var é possível redeclarar uma variável com o mesmo nome ao longo do código e isso é um comportamento indesejado
+// recomendado SEMPRE USAR LET
+
+console.log (a, b)
+
+const c = 5 // constante, não pode receber outro valor. Se não existir necessidade de alterar o valor ao longo do código, recomenda-se usar const ao invés de let

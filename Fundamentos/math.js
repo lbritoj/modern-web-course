3bbbe6c5f503a10a0b605c128c849fcf4a6a70f2
Leonardo Brito <leonardobritto.jr@gmail.com>
2024-03-30 20:50:50 -0400

    objeto Math

---
+++ Fundamentos/math.js
@@ -0,0 +1,6 @@
+const raio = 5.6 // definir o raio
+
+const area = Math.PI * Math.pow(raio,2) //calcula a aréa da circunferência
+
+console.log(area)
+console.log(typeof Math) // Math é um objeto JS

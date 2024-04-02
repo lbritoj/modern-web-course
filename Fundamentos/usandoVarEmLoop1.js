77a157acc9d80584a740bf821c553a350cb6addb
Leonardo Brito <leonardobritto.jr@gmail.com>
2024-04-02 04:58:12 -0400

    usando e entendendo o funcionamento do var em um laço de repetição

---
+++ Fundamentos/usandoVarEmLoop1.js
@@ -0,0 +1,7 @@
+for ( var i = 0; i < 10; i++){
+    console.log(i)
+}
+
+console.log('i =',i)
+
+//a variável i, como foi declarada com var, possui escopo global, portanto ficará acessível fora do laço de repetição

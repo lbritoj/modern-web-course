f8624b5afd5188e42b4c672f6844d16c372da9e8
Leonardo Brito <leonardobritto.jr@gmail.com>
2024-04-06 11:33:55 -0400

    atribuicao

---
+++ Fundamentos/atribuicao.js
@@ -0,0 +1,16 @@
+const a = 7
+let b = 3
+
+b += a //atribuicao aditiva
+console.log(b)
+
+b -= a //atribuicao subtrativa
+console.log(b)
+
+b *= 2 // atribuicao multiplicativa
+console.log(b)
+
+b /= 2 // atribuicao divisiva
+console.log(b)
+
+b %= 2 // b recebe o resto da divisao por 2 => atribuicao modular

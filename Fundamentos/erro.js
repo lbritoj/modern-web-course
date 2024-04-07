8fb83b8385bf7f94b85f8bd3456f9367424f813f
Leonardo Brito <leonardobritto.jr@gmail.com>
2024-04-07 12:05:51 -0400

    Tratamento de erro. Try/Catch/Throw

---
+++ Fundamentos/erro.js
@@ -0,0 +1,26 @@
+//try, catch e throw
+function tratarErroELancar(erro) {
+    // throw new Error('Nome não encontrado')
+    // throw 10
+    // throw true
+    // throw 'mensagem'
+    throw {
+        nome: erro.name,
+        msg: erro.message,
+        date: new Date
+    }
+}
+
+function imprimirNomeGritado(obj) {
+    try {
+        console.log(obj.name.toUpperCase() + '!!!')
+    } catch (e) {
+        tratarErroELancar(e)
+    } finally {
+        console.log('final')
+    }
+
+}
+
+const obj = { nome: 'Roberto' }
+imprimirNomeGritado(obj)
